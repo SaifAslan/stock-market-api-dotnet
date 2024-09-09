9a43fea41c8d335c227b3893cf1cb842502db79c
Saif Aslan <saifaslan24@gmail.com>
2024-09-09 15:24:32 +0300

    querying for stocks and bases for authentication and autherization

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Threading.Tasks;
 using api.data;
 using api.Dtos.Stock;
@@ -40,14 +41,26 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
-            var stocks =  _context.Stocks.Include(c => c.Comments).AsQueryable();
-            if(!string.IsNullOrWhiteSpace(query.CompanyName)){
-                stocks = stocks.Where(s=>s.CompanyName.Contains(query.CompanyName));
+            var stocks = _context.Stocks.Include(c => c.Comments).AsQueryable();
+            if (!string.IsNullOrWhiteSpace(query.CompanyName))
+            {
+                stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
-            if(!string.IsNullOrWhiteSpace(query.Symbol)){
-                stocks = stocks.Where(s=>s.Symbol.Contains(query.Symbol));
+            if (!string.IsNullOrWhiteSpace(query.Symbol))
+            {
+                stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
-            return await stocks.ToListAsync();
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 
         }
 
@@ -59,7 +72,7 @@ namespace api.Repository
 
         public async Task<bool> StockExists(int id)
         {
-            return await _context.Stocks.AnyAsync(s=> s.Id == id);
+            return await _context.Stocks.AnyAsync(s => s.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
